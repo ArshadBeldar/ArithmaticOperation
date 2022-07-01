e4637c2603754983d106c870d84e6b763e279e10
ArshadBeldar <arshadsk9527@gmail.com>
2022-07-01 17:37:57 +0530

    MODIFIED: Added new Addition Class

---
+++ src/com/infogalaxy/Addition.java
@@ -0,0 +1,6 @@
+package com.infogalaxy;
+public class Addition {
+    int add(int a,int b) {
+        return a+b;
+    }
+}
--- src/com/infogalaxy/Operation.java
+++ src/com/infogalaxy/Operation.java
@@ -1,7 +1,18 @@
 package com.infogalaxy;
 
+import java.util.Scanner;
+
 public class Operation {
     public static void main(String[] args) {
+        Addition addition = new Addition();
+        Scanner sc = new Scanner(System.in);
+        int a,b,c;
         System.out.println("WELCOME TO OPERATION");
+
+        System.out.println("Enterthe 2 Numbers:");
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = addition.add(a,b);
+        System.out.println("Addition="+c);
     }
 }
